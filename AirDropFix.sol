83ee8e7f9f16eec01a74fbf128d90f1d33be49d2
ikingqu <ikingqu@gmail.com>
2018-10-26 13:19:48 +0800

    兼容不符合ERC20规范的空投合约

--- AirDropFix.sol
+++ AirDropFix.sol
@@ -135,7 +135,8 @@ contract Ownable {
 contract ERC20Basic {
   function totalSupply() public view returns (uint256);
   function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+  // function transfer(address to, uint256 value) public returns (bool);
+  function transfer(address to, uint256 value) public;
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
